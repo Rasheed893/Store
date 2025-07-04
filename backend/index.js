edfc387db28dcdfca3a3714a2af3c373fcf3db01
Rasheed893 <106796468+Rasheed893@users.noreply.github.com>
2025-07-04 16:29:18 +0400

    final backend

---
+++ backend/index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const helmet = require("helmet");
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser()); // Middleware to read HTTP-only cookies
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "https://js.stripe.com",
+        "https://m.stripe.network",
+        "https://b.stripecdn.com",
+        "https://*.stripe.com",
+        "https://apis.google.com",
+      ],
+      frameSrc: [
+        "'self'",
+        "https://js.stripe.com",
+        "https://hooks.stripe.com",
+        "https://m.stripe.network",
+        "https://*.stripe.com",
+      ],
+      connectSrc: [
+        "'self'",
+        "http://localhost:5000",
+        "https://api.stripe.com",
+        "https://m.stripe.com",
+      ],
+      styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+      fontSrc: ["'self'", "https://fonts.gstatic.com"],
+      imgSrc: ["'self'", "data:", "https://*.stripe.com"],
+    },
+  })
+);
+
+// Routes
+const itemRoute = require("./src/item/item.route");
+const orderRoute = require("./src/orders/order.route");
+const userRoute = require("./src/users/user.route");
+const adminRoute = require("./src/stats/admin.stats");
+const emailRoute = require("./src/email/email.route");
+const commentRoute = require("./src/comments/comment.route");
+const spinnerRoute = require("./src/spinner/spinner.router");
+const paymentsRoute = require("./src/payment/payment.route");
+const shippingRate = require("./src/shipping/shippingRate.route");
+const promoCode = require("./src/promoCode/promoCode.router");
+
+app.use("/api/items", itemRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/auth", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/email", emailRoute);
+app.use("/invoices", express.static(path.join(__dirname, "src/invoices")));
+app.use("/api/comments", commentRoute);
+app.use("/api/spinner", spinnerRoute);
+app.use("/api/payments", paymentsRoute);
+app.use("/api/shipping-rate", shippingRate);
+app.use("/api/promo", promoCode);
+
+// eSd46z75rnuAJKEJ - password
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+
+  app.use("/", (req, res) => {
+    res.send({ message: "Hello from the server!" });
+  });
+}
+
+main()
+  .then(() => console.log("Mongodb connect successfully"))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
